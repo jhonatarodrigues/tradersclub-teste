ddbffea0cfe27c6d0f392324cd22f38579117254
jhonata rodrigues <jhonata.a.r@hotmail.com>
2020-11-05 01:12:43 -0300

    trata warnings

--- src/store/ducks/car/sagas.ts
+++ src/store/ducks/car/sagas.ts
@@ -1,11 +1,13 @@
 import { call, put } from 'redux-saga/effects';
 import api from '../../../services/api';
-import { loadSuccess, loadError } from './actions';
+import { loadError } from './actions';
 
 export function* load() {
   try {
     // -- chamada de api
     const response = yield call(api.get, '');
+    // eslint-disable-next-line no-console
+    console.log(response); // -- retorno que viria daa api e envia pro reducer.
 
     // yield put(loadSuccess(response.data));
   } catch (err) {
